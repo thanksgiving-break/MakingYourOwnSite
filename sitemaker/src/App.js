f143b16d3bdfece188736849dc9040d0170ed260
Michael Trevino <41596862+TrevMike@users.noreply.github.com>
2020-11-20 17:29:13 -0600

    dummyData added for testing (#5)

--- sitemaker/src/App.js
+++ sitemaker/src/App.js
@@ -1,6 +1,49 @@
 import React from "react";
-
+const dummyData = {
+  //header
+  headerStyle: null,
+  title: "Example Title",
+  //nav
+  links: [
+    { link: "Home" },
+    { link: "About Us" },
+    { link: "Product" },
+    { link: "Contact" },
+  ],
+  // section
+  sectionstyle: null,
+  photos: [
+    {
+      photo:
+        "https://images.pexels.com/photos/1091659/pexels-photo-1091659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+      photo:
+        "https://images.pexels.com/photos/2345374/pexels-photo-2345374.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+      photo:
+        "https://images.pexels.com/photos/5903191/pexels-photo-5903191.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+      photo:
+        "https://images.pexels.com/photos/4576085/pexels-photo-4576085.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+  ],
+  section1: "",
+  section2: "",
+  // footer
+  footerStyle: null,
+  contactAndTitle: "",
+  //address
+  address: "108 E Garcia St Pharr, Texas 78577",
+  email: "Preince560m@gmail.com",
+  phone: "(956)-222-2222",
+  icons: [{ iconImage: "", iconName: "" }],
+  copyRights: "" /*function that gets the date of that year */,
+};
 function App() {
+  console.log(dummyData); // Look at what you are getting back in the console
   return (
     <div className="App">
       <h1>Hello its working</h1>
