23c46be92574e937fb0f0162e4e48fb5c1100d44
declan-casey <dpcaseyboy99@gmail.com>
2020-11-22 00:53:59 -0500

    footer

---
+++ sitemaker/src/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+
+function Footer(props) {
+    const filteredNav = props.links.filter((item) => {
+        return item.link !== "";
+    });
+
+    return (
+        <div className = "foot">
+    {filteredNav.map((item, index)=>{
+     return ( <nav>
+            <a href = ".#" key = {index}>{item.link}</a>
+         </nav>
+         );
+        
+    })}
+    </div>
+    );
+}
+
+
+
+export default Footer
